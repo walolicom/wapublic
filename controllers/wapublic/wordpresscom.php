e1536c6b625e176e79465ba60cabc7a8be95ff16
latuminggi <git@latuminggi.com>
2014-09-22 09:21:11 +0700

    Update wordpresscom.php

--- controllers/wapublic/wordpresscom.php
+++ controllers/wapublic/wordpresscom.php
@@ -15,66 +15,67 @@ class Wordpresscom extends CI_Controller
         // Load Wapublic Wordpress.com API library
         $this->load->library( 'wapublic/wpcom_api' );
     }
-
-	// View site information
-	public function index(  )
-	{
-		$site = $this->wpcom_api->get_site();
-		echo '<pre>'; print_r( $site );
-	}
-
-	// View recent posts
-	public function blog( $page = '' )
-	{
-		$posts = $this->wpcom_api->get_posts( $page );
-		echo '<pre>'; print_r( $posts );
-	}
-
-	// View a post
-	public function post( $var )
-	{
-		// Load CodeIgniter url helper
-		$this->load->helper('url');
-
-		if ( is_numeric( $var ) ) { $get_by = 'ID'; } else { $get_by = 'slug'; }
-		$post = $this->wpcom_api->get_post( $get_by, $var );
-		if ( is_numeric( $var ) ) { $post_id = $var; } else { $post_id = $post->ID; }
-
-		// echo '<pre>'; print_r( $post );
-		echo 'Post ID: '. $post->ID .'<br>';
-		echo 'Post slug: '. $post->slug .'<br>';
-		echo 'Post title: '. $post->title .'<br>';
-		echo 'Post content: ' .$post->content .'<br>';
-		echo 'Comments open: '. $post->comments_open .'<br>';
-		echo 'Comments count: '. $post->comment_count .'<br>';
-
-		if ( $post->comments_open == 1 )
+    
+    // View site information
+    public function index(  )
+    {
+    	$site = $this->wpcom_api->get_site();
+    	echo '<pre>'; print_r( $site );
+    }
+    
+    // View recent posts
+    public function blog( $page = '' )
+    {
+    	$posts = $this->wpcom_api->get_posts( $page );
+    	echo '<pre>'; print_r( $posts );
+    }
+    
+    // View a post
+    public function post( $var )
+    {
+        // Load CodeIgniter url helper
+    	$this->load->helper('url');
+    	
+    	if ( is_numeric( $var ) ) { $get_by = 'ID'; } else { $get_by = 'slug'; }
+    	$post = $this->wpcom_api->get_post( $get_by, $var );
+    	if ( is_numeric( $var ) ) { $post_id = $var; } else { $post_id = $post->ID; }
+    	
+    	// echo '<pre>'; print_r( $post );
+    	echo 'Post ID: '.           $post->ID .'<br>';
+    	echo 'Post slug: '.         $post->slug .'<br>';
+    	echo 'Post title: '.        $post->title .'<br>';
+    	echo 'Post content: '.      $post->content .'<br>';
+    	echo 'Comments open: '.     $post->comments_open .'<br>';
+    	echo 'Comments count: '.    $post->comment_count .'<br>';
+    	
+    	if ( $post->comments_open == 1 )
 		{
-			echo '<br><a href="'. site_url( 'wordpresscom/input_comment/'. $var ) .'">';
-			echo 'Post a comment</a><br>';
-		}
-
-		$post_comments = $this->wpcom_api->get_comments_post( $post_id );
-		foreach ( $post_comments->comments as $comment )
+		    echo '<br><a href="'. site_url( 'wordpresscom/input_comment/'. $var ) .'">';
+		    echo 'Post a comment</a><br>';
+        }
+        
+        $post_comments = $this->wpcom_api->get_comments_post( $post_id );
+        
+        foreach ( $post_comments->comments as $comment )
 		{
-			echo '<br><a href="'. $comment->author->URL .'">'. $comment->author->name .'</a><br>';
-			echo $comment->content;
+		    echo '<br><a href="'. $comment->author->URL .'">'. $comment->author->name .'</a><br>';
+		    echo $comment->content;
 		}
 	}
-
+	
 	// View recent comments
 	public function recent_comments( $page = '' )
 	{
-		$recent_comments = $this->wpcom_api->get_comments_recent( $page );
-		echo '<pre>'; print_r( $recent_comments );
-	}
-
-	// View a post comments
-	public function post_comments( $var, $page = '' )
-	{
-		$post_comments = $this->wpcom_api->get_comments_post( $var, $page );
-		echo '<pre>'; print_r( $post_comments );
-	}
+	    $recent_comments = $this->wpcom_api->get_comments_recent( $page );
+	    echo '<pre>'; print_r( $recent_comments );
+    }
+    
+    // View a post comments
+    public function post_comments( $var, $page = '' )
+    {
+        $post_comments = $this->wpcom_api->get_comments_post( $var, $page );
+        echo '<pre>'; print_r( $post_comments );
+    }
 
 	// Get authentication code with Wordpress.com API
 	public function auth(  )
