f948d2df9cc43bedc88659cb8ec74800c3f9d060
latuminggi <git@latuminggi.com>
2014-09-23 11:06:26 +0700

    Update

--- application/controllers/wapublic/wordpresscom.php
+++ application/controllers/wapublic/wordpresscom.php
@@ -8,120 +8,120 @@
 
 class Wordpresscom extends CI_Controller
 {
-    function __construct(  )
-    {
-        parent::__construct();
-        
-        // Load Wapublic Wordpress.com API library
-        $this->load->library( 'wapublic/wpcom_api' );
-    }
+	function __construct(  )
+	{
+		parent::__construct();
+		
+		// Load Wapublic Wordpress.com API library
+		$this->load->library( 'wapublic/wpcom_api' );
+    	}
     
-    // View site information
-    public function index(  )
-    {
-    	$site = $this->wpcom_api->get_site();
-    	echo '<pre>'; print_r( $site );
-    }
-    
-    // View recent posts
-    public function blog( $page = '' )
-    {
-    	$posts = $this->wpcom_api->get_posts( $page );
-    	echo '<pre>'; print_r( $posts );
-    }
-    
-    // View a post
-    public function post( $var )
-    {
-        // Load CodeIgniter url helper
-    	$this->load->helper('url');
-    	
-    	if ( is_numeric( $var ) ) { $get_by = 'ID'; } else { $get_by = 'slug'; }
-    	$post = $this->wpcom_api->get_post( $get_by, $var );
-    	if ( is_numeric( $var ) ) { $post_id = $var; } else { $post_id = $post->ID; }
-    	
-    	// echo '<pre>'; print_r( $post );
-    	echo 'Post ID: '.           $post->ID .'<br>';
-    	echo 'Post slug: '.         $post->slug .'<br>';
-    	echo 'Post title: '.        $post->title .'<br>';
-    	echo 'Post content: '.      $post->content .'<br>';
-    	echo 'Comments open: '.     $post->comments_open .'<br>';
-    	echo 'Comments count: '.    $post->comment_count .'<br>';
-    	
-    	if ( $post->comments_open == 1 )
-    	{
-    	    echo '<br><a href="'. site_url( 'wordpresscom/input_comment/'. $var ) .'">';
-    	    echo 'Post a comment</a><br>';
-        }
+	// View site information
+	public function index(  )
+	{
+		$site = $this->wpcom_api->get_site();
+		echo '<pre>'; print_r( $site );
+	}
+	
+	// View recent posts
+	public function blog( $page = '' )
+	{
+		$posts = $this->wpcom_api->get_posts( $page );
+		echo '<pre>'; print_r( $posts );
+	}
+	
+	// View a post
+	public function post( $var )
+	{
+		// Load CodeIgniter url helper
+		$this->load->helper('url');
+		
+		if ( is_numeric( $var ) ) { $get_by = 'ID'; } else { $get_by = 'slug'; }
+		$post = $this->wpcom_api->get_post( $get_by, $var );
+		if ( is_numeric( $var ) ) { $post_id = $var; } else { $post_id = $post->ID; }
+		
+		// echo '<pre>'; print_r( $post );
+		echo 'Post ID: '.           $post->ID .'<br>';
+		echo 'Post slug: '.         $post->slug .'<br>';
+		echo 'Post title: '.        $post->title .'<br>';
+		echo 'Post content: '.      $post->content .'<br>';
+		echo 'Comments open: '.     $post->comments_open .'<br>';
+		echo 'Comments count: '.    $post->comment_count .'<br>';
+		
+		if ( $post->comments_open == 1 )
+		{
+		        echo '<br><a href="'. site_url( 'wordpresscom/input_comment/'. $var ) .'">';
+		        echo 'Post a comment</a><br>';
+		}
 	    
-        $post_comments = $this->wpcom_api->get_comments_post( $post_id );
+		$post_comments = $this->wpcom_api->get_comments_post( $post_id );
 	    
-        foreach ( $post_comments->comments as $comment )
-        {
-            echo '<br><a href="'. $comment->author->URL .'">'. $comment->author->name .'</a><br>';
-            echo $comment->content;
-        }
-    }
-    
-    // View recent comments
-    public function recent_comments( $page = '' )
-    {
-        $recent_comments = $this->wpcom_api->get_comments_recent( $page );
-        echo '<pre>'; print_r( $recent_comments );
-    }
-    
-    // View a post comments
-    public function post_comments( $var, $page = '' )
-    {
-        $post_comments = $this->wpcom_api->get_comments_post( $var, $page );
-        echo '<pre>'; print_r( $post_comments );
-    }
-    
-    // Get authentication code with Wordpress.com API
-    public function auth(  )
-    {
-        $auth_code = $this->input->get('code', true);
-        
-        if ( $auth_code == true )
-        {
-            $auth = $this->wpcom_api->get_auth( $auth_code );
-            echo 'Code: '. $auth_code; echo '<pre>'; print_r( $auth );
-        }
-        else
-        {
-            $auth_button = $this->wpcom_api->get_auth_button();
-            echo $auth_button;
-        }
-    }
-    
-    // Input comment on a post
-    public function input_comment( $var )
-    {
-        // Load CodeIgniter form validation library
-        $this->load->library('form_validation');
-        
-        // Load CodeIgniter form helper
-        $this->load->helper('form');
-        
-        if ( is_numeric( $var ) ) { $get_by = 'ID'; } else { $get_by = 'slug'; }
-        $post = $this->wpcom_api->get_post( $get_by, $var );
-        if ( is_numeric( $var ) ) { $post_id = $var; } else { $post_id = $post->ID; }
-        
-        echo 'Post title: '. $post->title .'<br>';
-        
-        if ( $post->comments_open == 1 )
-        {
-            echo form_open( $this->wpcom_api->post_comment_post_url( $post_id ) ) .'<br>';
-            echo form_input('email', '', 'placeholder="Email"') .'<br>';
-            echo form_input('author', '', 'placeholder="Name"') .'<br>';
-            echo form_input('url', '', 'placeholder="Website"') .'<br>';
-            echo form_textarea('content', '', 'placeholder="Comment"') .'<br>';
-            echo form_submit('submit', 'Post comment') .'<br>';
-            echo form_close();
-        }
-        
-        // $this->wpcom_api->post_comment_post( $var, $content );
-    }
+		foreach ( $post_comments->comments as $comment )
+		{
+			echo '<br><a href="'. $comment->author->URL .'">'. $comment->author->name .'</a><br>';
+			echo $comment->content;
+		}
+	}
+	
+	// View recent comments
+	public function recent_comments( $page = '' )
+	{
+		$recent_comments = $this->wpcom_api->get_comments_recent( $page );
+		echo '<pre>'; print_r( $recent_comments );
+	}
+	
+	// View a post comments
+	public function post_comments( $var, $page = '' )
+	{
+		$post_comments = $this->wpcom_api->get_comments_post( $var, $page );
+		echo '<pre>'; print_r( $post_comments );
+	}
+	
+	// Get authentication code with Wordpress.com API
+	public function auth(  )
+	{
+		$auth_code = $this->input->get('code', true);
+		
+		if ( $auth_code == true )
+		{
+			$auth = $this->wpcom_api->get_auth( $auth_code );
+			echo 'Code: '. $auth_code; echo '<pre>'; print_r( $auth );
+		}
+		else
+		{
+			$auth_button = $this->wpcom_api->get_auth_button();
+			echo $auth_button;
+		}
+	}
+	
+	// Input comment on a post
+	public function input_comment( $var )
+	{
+		// Load CodeIgniter form validation library
+		$this->load->library('form_validation');
+		
+		// Load CodeIgniter form helper
+		$this->load->helper('form');
+		
+		if ( is_numeric( $var ) ) { $get_by = 'ID'; } else { $get_by = 'slug'; }
+		$post = $this->wpcom_api->get_post( $get_by, $var );
+		if ( is_numeric( $var ) ) { $post_id = $var; } else { $post_id = $post->ID; }
+		
+		echo 'Post title: '. $post->title .'<br>';
+		
+		if ( $post->comments_open == 1 )
+		{
+			echo form_open( $this->wpcom_api->post_comment_post_url( $post_id ) ) .'<br>';
+			echo form_input('email', '', 'placeholder="Email"') .'<br>';
+			echo form_input('author', '', 'placeholder="Name"') .'<br>';
+			echo form_input('url', '', 'placeholder="Website"') .'<br>';
+			echo form_textarea('content', '', 'placeholder="Comment"') .'<br>';
+			echo form_submit('submit', 'Post comment') .'<br>';
+			echo form_close();
+		}
+		
+		// $this->wpcom_api->post_comment_post( $var, $content );
+	}
 }
 
 /* End of file wordpresscom.php */
--- application/libraries/wapublic/Wpcom_api.php
+++ application/libraries/wapublic/Wpcom_api.php
@@ -8,183 +8,183 @@
 
 class Wpcom_api
 {
-    function __construct(  )
-    {
-        // Wordpress.com REST API v.1 Service URL
-        $this->service_url          = 'https://public-api.wordpress.com/rest/v1';
-        
-        // Load CodeIgniter object
-        $this->CI =& get_instance();
-        
-        // Load wapublic config
-        $this->CI->load->config('wapublic');
-        
-        $this->app_name             = $this->CI->config->item('wpcom_app_name');
-        $this->client_id            = $this->CI->config->item('wpcom_client_id');
-        $this->client_secret        = $this->CI->config->item('wpcom_client_secret');
-        $this->auth_code            = $this->CI->config->item('wpcom_auth_code');
-        $this->login_url            = $this->CI->config->item('wpcom_login_url');
-        $this->redirect_url         = $this->CI->config->item('wpcom_redirect_url');
-        $this->request_token_url    = $this->CI->config->item('wpcom_request_token_url');
-        $this->authenticate_url     = $this->CI->config->item('wpcom_authenticate_url');
-        $this->site                 = $this->CI->config->item('wpcom_site');
-        $wpcom_tag_lang             = $this->CI->config->item('wpcom_tag_lang');
-        $wpcom_per_page             = $this->CI->config->item('wpcom_per_page');
-        
-        $this->site_info            = $this->get_site(); // wordpress.com site info
-        $this->site_id              = $this->site_info->ID; // wordpress.com site ID
-        $this->get_posts_url        = $this->site_info->meta->links->posts; // wordpress.com posts url
-        $this->get_comments_url     = $this->site_info->meta->links->comments; // wordpress.com comments url
-        
-        if ( $wpcom_tag_lang == '' ) { $this->lang = ''; }
-            else { $this->lang = 'tag='. $wpcom_tag_lang; }
-        if ( $wpcom_per_page == '' ) { $this->per_page = 'number=10'; }
-            else { $this->per_page = 'number='. $wpcom_per_page; }
-    }
-    
-    public function get_auth_button(  )
-    {
-        // Load CodeIgniter session library
-        $this->CI->load->library('session');
-        
-        $wpcc_state = md5( mt_rand() ); $this->CI->session->set_userdata( 'wpcc_state', $wpcc_state );
-        
-        $params     = array(
-                        'response_type' => 'code',
-                        'client_id' 	=> $this->client_id,
-                        'state' 	=> $wpcc_state,
-                        'redirect_uri' 	=> $this->redirect_url,
-                    );
-        $url_to     = $this->authenticate_url .'?'. http_build_query( $params );
-        
-        if ( $this->CI->input->get('error', true) == '' ) { $error_message = ''; }
-            else { $error_message = $this->CI->input->get('error_description', true); }
-            
-        $button     = '<h3>Connect to '. $this->app_name .'</h3>';
-        $button    .= '<p style="color:red">'. $error_message .'</p>';
-        $button    .= '<a href="'. $url_to .'"><img src="//s0.wp.com/i/wpcc-button.png" width="200" /></a>';
-        return $button;
-    }
-    
-    public function get_auth( $auth_code )
-    {
-        $wpcc_state	= $this->CI->session->userdata('wpcc_state');
-        $auth_code 	= $this->CI->input->get('code', true);
-        $auth_state	= $this->CI->input->get('state', true);
-        
-        if ( $auth_code == true )
+        function __construct(  )
         {
-            if ( $wpcc_state == '' )
-                { die( 'Warning! State variable missing after authentication.' ); }
-            if ( $auth_state != $wpcc_state )
-                { die( 'Warning! State mismatch. Authentication attempt may have been compromised.' ); }
+                // Wordpress.com REST API v.1 Service URL
+                $this->service_url              = 'https://public-api.wordpress.com/rest/v1';
                 
-            $curl   =   curl_init( $this->request_token_url );
-                        curl_setopt( $curl, CURLOPT_POST, true );
-                        curl_setopt( $curl, CURLOPT_POSTFIELDS, array(
-                                'client_id'     => $this->client_id,
-                                'redirect_uri'  => $this->redirect_url,
-                                'client_secret' => $this->client_secret,
-                                'code'          => $auth_code, // The code from the previous request
-                                'grant_type'    => 'authorization_code',
-                            )
-                        );
-                        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1);
-
-            $auth   =   curl_exec( $curl );
-            $secret =   json_decode( $auth );
-            return $secret;
+                // Load CodeIgniter object
+                $this->CI =& get_instance();
+                
+                // Load wapublic config
+                $this->CI->load->config('wapublic');
+                
+                $this->app_name                 = $this->CI->config->item('wpcom_app_name');
+                $this->client_id                = $this->CI->config->item('wpcom_client_id');
+                $this->client_secret            = $this->CI->config->item('wpcom_client_secret');
+                $this->auth_code                = $this->CI->config->item('wpcom_auth_code');
+                $this->login_url                = $this->CI->config->item('wpcom_login_url');
+                $this->redirect_url             = $this->CI->config->item('wpcom_redirect_url');
+                $this->request_token_url        = $this->CI->config->item('wpcom_request_token_url');
+                $this->authenticate_url         = $this->CI->config->item('wpcom_authenticate_url');
+                $this->site                     = $this->CI->config->item('wpcom_site');
+                $wpcom_tag_lang                 = $this->CI->config->item('wpcom_tag_lang');
+                $wpcom_per_page                 = $this->CI->config->item('wpcom_per_page');
+                
+                $this->site_info                = $this->get_site(); // wordpress.com site info
+                $this->site_id                  = $this->site_info->ID; // wordpress.com site ID
+                $this->get_posts_url            = $this->site_info->meta->links->posts; // wordpress.com posts url
+                $this->get_comments_url         = $this->site_info->meta->links->comments; // wordpress.com comments url
+                
+                if ( $wpcom_tag_lang == '' ) { $this->lang = ''; }
+                        else { $this->lang = 'tag='. $wpcom_tag_lang; }
+                if ( $wpcom_per_page == '' ) { $this->per_page = 'number=10'; }
+                        else { $this->per_page = 'number='. $wpcom_per_page; }
         }
         
-        // Redirect errors or cancelled requests back to login page
-        header( 'Location: '. $this->login_url ); die();
-    }
-    
-    // Get site information
-    public function get_site(  )
-    {
-        $get_site_uri   = '/sites/'. $this->site;
-        $options        = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
-        $context        = stream_context_create( $options );
-        $response       = file_get_contents( $this->service_url . $get_site_uri, false, $context );
-        $result         = json_decode( $response );
-        return $result;
-    }
-    
-    // Get recent posts
-    public function get_posts( $page = '' )
-    {
-        $get_posts_uri  = $this->get_posts_url .'?'. $this->per_page .'&'. $this->lang;
-        if ( $page != '' ) { $get_posts_uri .= '&page='. $page; }
-        $options        = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
-        $context        = stream_context_create( $options );
-        $response       = file_get_contents( $get_posts_uri, false, $context );
-        $result         = json_decode( $response );
-        return $result;
-    }
-    
-    // Get a post (whether by post's slug or ID)
-    public function get_post( $get_by = 'slug', $var )
-    {
-        $get_post_uri   = $this->get_posts_url .'slug:'. $var;
-        if ( $get_by != 'slug' ) { $get_post_uri = $this->get_posts_url . $var; }
-        $options        = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
-        $context        = stream_context_create( $options );
-        $response       = file_get_contents( $get_post_uri, false, $context );
-        $result         = json_decode( $response );
-        return $result;
-    }
-    
-    // Get recent comments
-    public function get_comments_recent( $page = '' )
-    {
-        $get_comments_uri   = $this->get_comments_url .'?'. $this->per_page;
-        if ( $page != '' ) { $get_comments_uri .= '&page='. $page; }
-        $options            = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
-        $context            = stream_context_create( $options );
-        $response           = file_get_contents( $get_comments_uri, false, $context );
-        $result             = json_decode( $response );
-        return $result;
-    }
-    
-    // Get a post comments
-    public function get_comments_post( $var, $page = '' )
-    {
-        $get_comments_uri   = $this->get_posts_url . $var .'/replies/?'. $this->per_page;
-        if ( $page != '' ) { $get_comments_uri .= '&page='. $page; }
-        $options            = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
-        $context            = stream_context_create( $options );
-        $response           = file_get_contents( $get_comments_uri, false, $context );
-        $result             = json_decode( $response );
-        return $result;
-    }
-    
-    // Post a comment on a post service url
-    public function post_comment_post_url( $var )
-    {
-        $post_comment_uri   = $this->get_posts_url . $var .'/replies/new';
-        return $post_comment_uri;
-    }
-    
-    // Post a comment on a post
-    public function post_comment_post( $var, $content )
-    {
-        $post_comment_uri   = $this->get_posts_url . $var .'/replies/new';
-        $options            = array(
-                                'http' => array(
-                                    'method' => 'POST', 'ignore_errors' => true,
-                                    'header' => array(
-                                        0 => 'authorization: Bearer '. $token,
-                                        1 => 'Content-Type: application/x-www-form-urlencoded'
-                                    ),
-                                    'content' => http_build_query( array( 'content' => $content ) ),
-                                )
-                            );
-        $context            = stream_context_create( $options );
-        $response           = file_get_contents( $post_comment_uri, false, $context );
-        $result             = json_decode( $response );
-        return $result;
-    }
+        public function get_auth_button(  )
+        {
+                // Load CodeIgniter session library
+                $this->CI->load->library('session');
+                
+                $wpcc_state     = md5( mt_rand() ); $this->CI->session->set_userdata( 'wpcc_state', $wpcc_state );
+                
+                $params         = array(
+                                        'response_type' => 'code',
+                                        'client_id'     => $this->client_id,
+                                        'state'         => $wpcc_state,
+                                        'redirect_uri'  => $this->redirect_url,
+                                );
+                $url_to         = $this->authenticate_url .'?'. http_build_query( $params );
+                
+                if ( $this->CI->input->get('error', true) == '' ) { $error_message = ''; }
+                        else { $error_message = $this->CI->input->get('error_description', true); }
+                    
+                $button         = '<h3>Connect to '. $this->app_name .'</h3>';
+                $button        .= '<p style="color:red">'. $error_message .'</p>';
+                $button        .= '<a href="'. $url_to .'"><img src="//s0.wp.com/i/wpcc-button.png" width="200" /></a>';
+                return $button;
+        }
+        
+        public function get_auth( $auth_code )
+        {
+                $wpcc_state     = $this->CI->session->userdata('wpcc_state');
+                $auth_code      = $this->CI->input->get('code', true);
+                $auth_state     = $this->CI->input->get('state', true);
+        
+                if ( $auth_code == true )
+                {
+                        if ( $wpcc_state == '' )
+                                { die( 'Warning! State variable missing after authentication.' ); }
+                        if ( $auth_state != $wpcc_state )
+                                { die( 'Warning! State mismatch. Authentication attempt may have been compromised.' ); }
+                        
+                        $curl   = curl_init( $this->request_token_url );
+                                  curl_setopt( $curl, CURLOPT_POST, true );
+                                  curl_setopt( $curl, CURLOPT_POSTFIELDS, array(
+                                                'client_id'     => $this->client_id,
+                                                'redirect_uri'  => $this->redirect_url,
+                                                'client_secret' => $this->client_secret,
+                                                'code'          => $auth_code, // The code from the previous request
+                                                'grant_type'    => 'authorization_code',
+                                        )
+                                  );
+                                  curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1);
+                        
+                        $auth   = curl_exec( $curl );
+                        $secret = json_decode( $auth );
+                        return $secret;
+                }
+                
+                // Redirect errors or cancelled requests back to login page
+                header( 'Location: '. $this->login_url ); die();
+        }
+        
+        // Get site information
+        public function get_site(  )
+        {
+                $get_site_uri   = '/sites/'. $this->site;
+                $options        = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
+                $context        = stream_context_create( $options );
+                $response       = file_get_contents( $this->service_url . $get_site_uri, false, $context );
+                $result         = json_decode( $response );
+                return $result;
+        }
+        
+        // Get recent posts
+        public function get_posts( $page = '' )
+        {
+                $get_posts_uri  = $this->get_posts_url .'?'. $this->per_page .'&'. $this->lang;
+                if ( $page != '' ) { $get_posts_uri .= '&page='. $page; }
+                $options        = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
+                $context        = stream_context_create( $options );
+                $response       = file_get_contents( $get_posts_uri, false, $context );
+                $result         = json_decode( $response );
+                return $result;
+        }
+        
+        // Get a post (whether by post's slug or ID)
+        public function get_post( $get_by = 'slug', $var )
+        {
+                $get_post_uri   = $this->get_posts_url .'slug:'. $var;
+                if ( $get_by != 'slug' ) { $get_post_uri = $this->get_posts_url . $var; }
+                $options        = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
+                $context        = stream_context_create( $options );
+                $response       = file_get_contents( $get_post_uri, false, $context );
+                $result         = json_decode( $response );
+                return $result;
+        }
+        
+        // Get recent comments
+        public function get_comments_recent( $page = '' )
+        {
+                $get_comments_uri       = $this->get_comments_url .'?'. $this->per_page;
+                if ( $page != '' ) { $get_comments_uri .= '&page='. $page; }
+                $options                = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
+                $context                = stream_context_create( $options );
+                $response               = file_get_contents( $get_comments_uri, false, $context );
+                $result                 = json_decode( $response );
+                return $result;
+        }
+        
+        // Get a post comments
+        public function get_comments_post( $var, $page = '' )
+        {
+                $get_comments_uri       = $this->get_posts_url . $var .'/replies/?'. $this->per_page;
+                if ( $page != '' ) { $get_comments_uri .= '&page='. $page; }
+                $options                = array( 'http' => array( 'method' => 'GET', 'ignore_errors' => true ) );
+                $context                = stream_context_create( $options );
+                $response               = file_get_contents( $get_comments_uri, false, $context );
+                $result                 = json_decode( $response );
+                return $result;
+        }
+        
+        // Post a comment on a post service url
+        public function post_comment_post_url( $var )
+        {
+                $post_comment_uri       = $this->get_posts_url . $var .'/replies/new';
+                return $post_comment_uri;
+        }
+        
+        // Post a comment on a post
+        public function post_comment_post( $var, $content )
+        {
+                $post_comment_uri       = $this->get_posts_url . $var .'/replies/new';
+                $options                = array(
+                                                'http' => array(
+                                                        'method' => 'POST', 'ignore_errors' => true,
+                                                        'header' => array(
+                                                                0 => 'authorization: Bearer '. $token,
+                                                                1 => 'Content-Type: application/x-www-form-urlencoded'
+                                                        ),
+                                                        'content' => http_build_query( array( 'content' => $content ) ),
+                                                )
+                                        );
+                $context                = stream_context_create( $options );
+                $response               = file_get_contents( $post_comment_uri, false, $context );
+                $result                 = json_decode( $response );
+                return $result;
+        }
 }
 
 /* End of file Wpcom_api.php */
